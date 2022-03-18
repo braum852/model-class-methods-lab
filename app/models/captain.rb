24bb155791e30f6bab4f6d90d65308ff6e1f51eb
braum852 <julian.teo59@yahoo.com>
2022-03-19 00:03:14 +1000

    Done

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,23 +2,28 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    # includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+    #returns all captains of catamarans
   end
 
   def self.sailors
-    # includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+    #returns captains with sailboats
   end
 
   def self.motorboat_operators
-    # includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    #method inclusive for method below
   end
 
   def self.talented_seafarers
-    # where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+    #returns captains of motorboats and sailboats
   end
 
   def self.non_sailors
-    # where.not("id IN (?)", self.sailors.pluck(:id))
+    where.not("id IN (?)", self.sailors.pluck(:id))
+    #returns people who are not captains of sailboats
   end
 
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -3,11 +3,13 @@ class Classification < ActiveRecord::Base
   has_many :boats, through: :boat_classifications
 
   def self.my_all
-    # all
+    all
+    #return all classifications
   end
 
   def self.longest
-    # Boat.longest.classifications
+    Boat.longest.classifications
+    #returns classifications for the longest boats
   end
 
 end
